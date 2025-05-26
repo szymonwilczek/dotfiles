44222f3e50188c12e17e70eb0257d73bebd0045a
Szymon Wilczek <kazikwilczek7@gmail.com>
2025-05-26 15:47:33 +0200

    sync dotfiles from arch

---
+++ Arch/wal/colors-wal-dmenu.h
@@ -0,0 +1,6 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#d0dae9", "#0f1018" },
+	[SchemeSel] = { "#d0dae9", "#606B86" },
+	[SchemeOut] = { "#d0dae9", "#9CB0DA" },
+};
