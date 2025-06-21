83f2ed4c829c31d6e83b20e957418de51debc12f
Szymon Wilczek <kazikwilczek7@gmail.com>
2025-06-21 15:36:35 +0200

    configuration update (for laptop)

--- AsusLaptop/nvim/lua/chadrc.lua
+++ AsusLaptop/nvim/lua/chadrc.lua
@@ -1,47 +1,54 @@
--- This file needs to have same structure as nvconfig.lua 
--- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
-
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "kanagawa-dragon",
-  	transparency = true
+  theme = "rosepine",
+  transparency = true,
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  -- hl_override = {
+  -- 	Comment = { italic = true },
+  -- 	["@comment"] = { italic = true },
+  -- },
 }
 
-M.nvdash = { 
-  load_on_startup = true, 
+M.nvdash = {
+  load_on_startup = true,
   header = {
-      "                                                                       ",
-			"                                                                       ",
-			"                                                                       ",
-			"                                                                       ",
-			"                                                                       ",
-			"                                                                     ",
-			"       ████ ██████           █████      ██                     ",
-			"      ███████████             █████                             ",
-			"      █████████ ███████████████████ ███   ███████████   ",
-			"     █████████  ███    █████████████ █████ ██████████████   ",
-			"    █████████ ██████████ █████████ █████ █████ ████ █████   ",
-			"  ███████████ ███    ███ █████████ █████ █████ ████ █████  ",
-			" ██████  █████████████████████ ████ █████ █████ ████ ██████ ",
-			"                                                                       ",
-			"                                                                       ",
-			"                                                                       ",
-			"                                                                       ",
-			"                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                     ",
+    "       ████ ██████           █████      ██                     ",
+    "      ███████████             █████                             ",
+    "      █████████ ███████████████████ ███   ███████████   ",
+    "     █████████  ███    █████████████ █████ ██████████████   ",
+    "    █████████ ██████████ █████████ █████ █████ ████ █████   ",
+    "  ███████████ ███    ███ █████████ █████ █████ ████ █████  ",
+    " ██████  █████████████████████ ████ █████ █████ ████ ██████ ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
   },
 }
--- M.ui = {
---       tabufline = {
---          lazyload = false
---      }
---}
+M.ui = {
+  statusline = {
+    theme = "vscode_colored",
+  },
+}
+
+M.lsp = {
+  signature = true
+}
+
+M.colorify = {
+  enabled = true,
+  mode = "virtual",
+  virt_text = "󱓻 ",
+  highlight = { hex = true, lspvars = true },
+}
 
 return M
