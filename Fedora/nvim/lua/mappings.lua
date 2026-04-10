33c6db68f9f1658ecedb9b82755c4d591a4439b8
Szymon Wilczek <swilczek.lx@gmail.com>
2026-04-10 19:22:19 +0200

    [FEDORA]: nvim & tmux sync
    Signed-off-by: Szymon Wilczek <swilczek.lx@gmail.com>

--- Fedora/nvim/lua/mappings.lua
+++ Fedora/nvim/lua/mappings.lua
@@ -5,11 +5,6 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
-map("t", "<C-h>", "<C-\\><C-n><C-w>h", { desc = "Terminal Left window focus" })
-map("t", "<C-l>", "<C-\\><C-n><C-w>l", { desc = "Terminal Right window focus" })
-map("t", "<C-j>", "<C-\\><C-n><C-w>j", { desc = "Terminal Down window focus" })
-map("t", "<C-k>", "<C-\\><C-n><C-w>k", { desc = "Terminal Up window focus" })
-
 map("n", "<leader>tt", function()
   require("base46").toggle_theme()
 end, { desc = "Switch Theme" })
