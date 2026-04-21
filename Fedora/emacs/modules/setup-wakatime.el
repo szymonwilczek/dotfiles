68a25d0bfcaa92fd885d015a1bfd187a9a29aa1b
Szymon Wilczek <swilczek.lx@gmail.com>
2026-04-21 22:47:29 +0200

    main: remove Arch and Mint folders from main branch
    Signed-off-by: Szymon Wilczek <swilczek.lx@gmail.com>

--- Fedora/emacs/modules/setup-wakatime.el
+++
@@ -1,6 +0,0 @@
-(use-package wakatime-mode
-  :ensure t
-  :config
-  (global-wakatime-mode))
-
-(provide 'setup-wakatime)
--- Fedora/hyprland/waybar/scripts/power.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-opcja=$(echo -e "  Wyłącz\n  Uruchom ponownie\n󰍃  Wyloguj" | wofi --dmenu --width 250 --height 210 --prompt "Zasilanie" --cache-file /dev/null)
-
-case $opcja in
-    "  Wyłącz") systemctl poweroff ;;
-    "  Uruchom ponownie") systemctl reboot ;;
-    "󰍃  Wyloguj") hyprctl dispatch exit ;;
-esac
