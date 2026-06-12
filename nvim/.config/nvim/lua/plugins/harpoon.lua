6d55a89b3a172825cc60b115091f1f0d3f689067
Szymon Wilczek <swilczek.lx@gmail.com>
2026-06-12 14:29:14 +0200

    nvim: harpoon plugin
    Signed-off-by: Szymon Wilczek <swilczek.lx@gmail.com>

---
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local harpoon = require 'harpoon'
+      harpoon:setup()
+
+      -- Adding file to list
+      vim.keymap.set('n', '<leader>a', function() harpoon:list():add() end, { desc = 'Harpoon Add File' })
+      -- Openingn quick menu list
+      vim.keymap.set('n', '<C-e>', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = 'Harpoon Toggle Menu' })
+
+      -- Switching between list positions
+      vim.keymap.set('n', '<leader>1', function() harpoon:list():select(1) end, { desc = 'Harpoon File 1' })
+      vim.keymap.set('n', '<leader>2', function() harpoon:list():select(2) end, { desc = 'Harpoon File 2' })
+      vim.keymap.set('n', '<leader>3', function() harpoon:list():select(3) end, { desc = 'Harpoon File 3' })
+      vim.keymap.set('n', '<leader>4', function() harpoon:list():select(4) end, { desc = 'Harpoon File 4' })
+    end,
+  },
+}
