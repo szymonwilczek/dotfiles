262d3ac391b7b18789bbdcfd298000f1553f9502
Szymon Wilczek <swilczek.lx@gmail.com>
2026-06-12 14:27:25 +0200

    nvim: remove mini.pairs
    I was soooo frustrated using this.
    
    Signed-off-by: Szymon Wilczek <swilczek.lx@gmail.com>

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -135,7 +135,7 @@ return {
         n_lines = 500,
       }
       require('mini.surround').setup()
-      require('mini.pairs').setup()
+      -- require('mini.pairs').setup()
 
       local statusline = require 'mini.statusline'
       statusline.setup { use_icons = vim.g.have_nerd_font }
