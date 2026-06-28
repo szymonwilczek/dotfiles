2716bce6c8cb3b8b853591300c52b9c27d600db6
Szymon Wilczek <swilczek.lx@gmail.com>
2026-06-28 22:53:43 +0200

    lazygit: fix for line editing
    Signed-off-by: Szymon Wilczek <swilczek.lx@gmail.com>

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -57,12 +57,6 @@ return {
       },
       lazygit = {
         configure = true,
-        config = {
-          os = {
-            edit = '[ -z "$NVIM" ] && (nvim -- {{filename}}) || (nvim --server "$NVIM" --remote-send "q" && nvim --server "$NVIM" --remote {{filename}})',
-            editAtLine = '[ -z "$NVIM" ] && (nvim +{{line}} -- {{filename}}) || (nvim --server "$NVIM" --remote-send "q" && nvim --server "$NVIM" --remote {{filename}} && nvim --server "$NVIM" --remote-send ":{{line}}<CR>")',
-          },
-        },
       },
     },
     keys = {
