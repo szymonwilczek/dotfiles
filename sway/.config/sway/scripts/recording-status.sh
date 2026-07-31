db7d251bbc7ef38ad0427b7b9dcdb420682771d3
Szymon Wilczek <swilczek.lx@gmail.com>
2026-07-31 23:43:35 +0200

    gromit: remove indicator from script
    It already has a tray icon, second one is not necessary.
    
    Signed-off-by: Szymon Wilczek <swilczek.lx@gmail.com>

--- sway/.config/sway/scripts/recording-status.sh
+++ sway/.config/sway/scripts/recording-status.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
-IS_REC=0
-IS_DRAW=0
-
 if pgrep -x wl-screenrec >/dev/null; then
-    IS_REC=1
-fi
-
-if pgrep -i gromit-mpx >/dev/null; then
-    IS_DRAW=1
-fi
-
-if [ $IS_REC -eq 1 ] && [ $IS_DRAW -eq 1 ]; then
-    echo '{"text": "🔴 ✏️", "class": "active"}'
-elif [ $IS_REC -eq 1 ]; then
     echo '{"text": "🔴", "class": "recording"}'
-elif [ $IS_DRAW -eq 1 ]; then
-    echo '{"text": "✏️", "class": "drawing"}'
 else
     echo '{"text": "", "class": "idle"}'
 fi
