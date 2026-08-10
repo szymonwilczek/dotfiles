6cf185a1f6e29765e3f5b79a6669e679e9ce03f8
Szymon Wilczek <swilczek.lx@gmail.com>
2026-08-10 14:27:59 +0200

    nvim: skimmed bufferline
    Signed-off-by: Szymon Wilczek <swilczek.lx@gmail.com>

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -4,41 +4,17 @@ return {
     version = '*',
     event = 'VeryLazy',
     dependencies = 'nvim-tree/nvim-web-devicons',
-    config = function()
-      local function sync_offset_highlights()
-        local dir_fg = vim.api.nvim_get_hl(0, { name = 'NeoTreeDirectoryIcon', link = false }).fg
-        local neotree_bg = vim.api.nvim_get_hl(0, { name = 'NeoTreeNormal', link = false }).bg or vim.api.nvim_get_hl(0, { name = 'Normal', link = false }).bg
-        local sep_fg = vim.api.nvim_get_hl(0, { name = 'WinSeparator', link = false }).fg
-        vim.api.nvim_set_hl(0, 'NeoTreeOffsetTitle', { fg = dir_fg, bg = neotree_bg })
-        vim.api.nvim_set_hl(0, 'BufferLineOffsetSeparator', { fg = sep_fg, bg = neotree_bg })
-      end
-
-      require('bufferline').setup {
-        options = {
-          mode = 'buffers',
-          separator_style = 'thin',
-          show_buffer_close_icons = false,
-          show_close_icon = false,
-          offsets = {
-            {
-              filetype = 'neo-tree',
-              text = function() return '  ' .. vim.fn.fnamemodify(vim.fn.getcwd(), ':t') end,
-              highlight = 'NeoTreeOffsetTitle',
-              text_align = 'center',
-              separator = '| ',
-            },
-          },
-        },
-      }
-      sync_offset_highlights()
-      vim.api.nvim_create_autocmd({ 'ColorScheme', 'VimEnter' }, { callback = sync_offset_highlights })
-      vim.api.nvim_create_autocmd('FileType', {
-        pattern = 'neo-tree',
-        callback = function() vim.schedule(sync_offset_highlights) end,
-      })
-
-      vim.keymap.set('n', '<TAB>', ':BufferLineCycleNext<CR>', { desc = 'Następna zakładka' })
-      vim.keymap.set('n', '<S-TAB>', ':BufferLineCyclePrev<CR>', { desc = 'Poprzednia zakładka' })
-    end,
+    opts = {
+      options = {
+        mode = 'buffers',
+        separator_style = 'thin',
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+      },
+    },
+    keys = {
+      { '<TAB>', '<cmd>BufferLineCycleNext<CR>', desc = 'Następna zakładka' },
+      { '<S-TAB>', '<cmd>BufferLineCyclePrev<CR>', desc = 'Poprzednia zakładka' },
+    },
   },
 }
