a5d81febec2829987bdb66fcab5d3beae8d59dac
Szymon Wilczek <swilczek.lx@gmail.com>
2026-08-18 02:03:49 +0200

    emacs: fix for formatting buffers without eglot
    Signed-off-by: Szymon Wilczek <swilczek.lx@gmail.com>

--- emacs/.config/emacs/modules/lsp/lsp-keys.el
+++ emacs/.config/emacs/modules/lsp/lsp-keys.el
@@ -1,5 +1,18 @@
 ;;; LSP and diagnostic keybindings
 
+(defun my/format-buffer ()
+  "Format buffer with Eglot if active, or fallback to indent-region."
+  (interactive)
+  (require 'eglot nil t)
+  (cond
+   ((and (bound-and-true-p eglot--managed-mode) (fboundp 'eglot-format-buffer))
+    (eglot-format-buffer))
+   ((and (bound-and-true-p eglot--managed-mode) (fboundp 'eglot-format))
+    (eglot-format))
+   (t
+    (indent-region (point-min) (point-max))
+    (message "Formatted buffer with indent-region."))))
+
 (with-eval-after-load 'evil
 
   ;; Definition & Reference jumps
@@ -23,11 +36,11 @@
       "c"  '(:ignore t :which-key "Code")
       "ca" '(eglot-code-actions :which-key "Code Action")
       "cr" '(eglot-rename :which-key "Rename Symbol")
-      "cf" '(eglot-format :which-key "Format Buffer")
+      "cf" '(my/format-buffer :which-key "Format Buffer")
       "rn" '(eglot-rename :which-key "Rename Symbol")
 
       ;; Format shortcut
-      "fm" '(eglot-format :which-key "Format Buffer")
+      "fm" '(my/format-buffer :which-key "Format Buffer")
 
       ;; Diagnostics
       "dq" '(flymake-show-buffer-diagnostics :which-key "Diagnostics List"))))
