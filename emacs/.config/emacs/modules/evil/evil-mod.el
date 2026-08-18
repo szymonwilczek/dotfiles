101116b881cf305d98539fa422dcd65d3724f3d2
Szymon Wilczek <swilczek.lx@gmail.com>
2026-08-18 02:59:23 +0200

    emacs: highlight on yank
    Signed-off-by: Szymon Wilczek <swilczek.lx@gmail.com>

--- emacs/.config/emacs/modules/evil/evil-mod.el
+++ emacs/.config/emacs/modules/evil/evil-mod.el
@@ -6,7 +6,22 @@
         evil-want-C-u-scroll t
         evil-undo-system 'undo-redo)
   :config
-  (evil-mode 1))
+  (evil-mode 1)
+
+  ;; Highlight on yank
+  (defun my/evil-highlight-yank (orig-fn beg end &rest args)
+    (let ((res (apply orig-fn beg end args)))
+      (when (and (number-or-marker-p beg) (number-or-marker-p end))
+        (let ((ov (make-overlay beg end)))
+          (overlay-put ov 'face 'highlight)
+          (overlay-put ov 'priority 1000)
+          (run-at-time 0.2 nil
+                       (lambda (overlay)
+                         (when (overlay-buffer overlay)
+                           (delete-overlay overlay)))
+                       ov)))
+      res))
+  (advice-add 'evil-yank :around #'my/evil-highlight-yank))
 
 (use-package evil-collection
   :after evil
