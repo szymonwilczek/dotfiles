4d1c9e6b433837f2d7cbeb0087f8bd4c5a8e865c
Szymon Wilczek <swilczek.lx@gmail.com>
2026-08-20 15:25:11 +0200

    emacs: tabs: using my bufferline package
    Signed-off-by: Szymon Wilczek <swilczek.lx@gmail.com>

---
+++ emacs/.config/emacs/modules/tabs/tabs-keys.el
@@ -0,0 +1,17 @@
+(with-eval-after-load 'evil
+  (define-key evil-normal-state-map (kbd "TAB") #'bufferline-next-tab)
+  (define-key evil-normal-state-map (kbd "<tab>") #'bufferline-next-tab)
+  (define-key evil-normal-state-map (kbd "<backtab>") #'bufferline-prev-tab)
+  (define-key evil-normal-state-map (kbd "S-TAB") #'bufferline-prev-tab)
+  (define-key evil-normal-state-map (kbd "<S-tab>") #'bufferline-prev-tab))
+
+(with-eval-after-load 'evil-keys
+  (when (fboundp 'my-leader-def)
+    (my-leader-def
+      "b"  '(:ignore t :which-key "Buffers")
+      "bp" '(bufferline-pick :which-key "Pick Buffer")
+      "bd" '(bufferline-pick-close :which-key "Pick Close Buffer")
+      "bc" '(bufferline-pick-close :which-key "Pick Close Buffer")
+      "bP" '(bufferline-toggle-pin :which-key "Toggle Pin Buffer"))))
+
+(provide 'tabs-keys)
---
+++ emacs/.config/emacs/modules/tabs/tabs-mod.el
@@ -0,0 +1,11 @@
+(use-package bufferline
+  :load-path "~/Dokumenty/GitHub/bufferline.el"
+  :demand t
+  :custom
+  (bufferline-separator-style 'vertical)
+  :config
+  (global-bufferline-mode 1))
+
+(require 'tabs-keys)
+
+(provide 'tabs-mod)
