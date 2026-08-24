cafbe06905fa9324973f80653003edd45dd7b44a
Szymon Wilczek <swilczek.lx@gmail.com>
2026-08-24 15:56:54 +0200

    emacs: init: sync
    Signed-off-by: Szymon Wilczek <swilczek.lx@gmail.com>

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -21,3 +21,24 @@
 (require 'writings-mod)
 
 (provide 'init)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(global-wakatime-mode t)
+ '(package-selected-packages
+   '(apheleia auctex-latexmk cape citar company-auctex company-reftex
+              consult corfu dashboard diff-hl doom-themes eat
+              ef-themes evil-collection evil-org evil-surround forge
+              general ghostel lsp-ui marginalia nerd-icons-completion
+              notmuch orderless org-roam org-superstar org-tree-slide
+              persp-projectile texfrag treemacs-evil
+              treemacs-nerd-icons treemacs-perspective
+              treemacs-projectile vertico vterm wakatime-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
