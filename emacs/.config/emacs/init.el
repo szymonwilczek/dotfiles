2ff5b76e23ef23d470d2a6ff5a8f5ac97f5ad93b
Szymon Wilczek <swilczek.lx@gmail.com>
2026-08-25 13:45:03 +0200

    emacs: init: sync
    Signed-off-by: Szymon Wilczek <swilczek.lx@gmail.com>

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -40,9 +40,9 @@
                  company-auctex company-reftex consult corfu dashboard
                  diff-hl doom-themes eat ef-themes evil-collection
                  evil-org evil-surround forge general ghostel
-                 git-gutter git-gutter-fringe lsp-ui marginalia
-                 nerd-icons-completion nerd-icons-corfu notmuch
-                 olivetti orderless org-roam org-superstar
+                 git-gutter git-gutter-fringe good-scroll lsp-ui
+                 marginalia nerd-icons-completion nerd-icons-corfu
+                 notmuch olivetti orderless org-roam org-superstar
                  org-tree-slide pdf-tools persp-projectile plan-polsl
                  texfrag treemacs-evil treemacs-nerd-icons
                  treemacs-perspective treemacs-projectile vertico
