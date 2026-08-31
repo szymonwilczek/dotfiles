97580b440cd29b70cac824cd78884f845d78a0ac
Szymon Wilczek <swilczek.lx@gmail.com>
2026-08-31 14:45:20 +0200

    emacs: jot: fix custom quote hazards and path initialization
    Signed-off-by: Szymon Wilczek <swilczek.lx@gmail.com>

--- emacs/.config/emacs/modules/jot/jot-mod.el
+++ emacs/.config/emacs/modules/jot/jot-mod.el
@@ -4,15 +4,15 @@
   :load-path "~/Dokumenty/GitHub/jot.el"
   :demand t
   :custom
-  (jot-dir (expand-file-name "~/.local/share/tmux-jot"))
   (jot-extension "md")
-  (jot-default-mode 'markdown-mode)
-  (jot-session-backend 'auto)
-  (jot-popup-x 'right)
+  (jot-default-mode markdown-mode)
+  (jot-session-backend auto)
+  (jot-popup-x right)
   (jot-popup-y 0)
   (jot-popup-width 0.40)
   (jot-popup-height 0.50)
   :config
+  (setq jot-dir (expand-file-name "~/.local/share/tmux-jot"))
   (jot-mode 1))
 
 (require 'jot-keys)
