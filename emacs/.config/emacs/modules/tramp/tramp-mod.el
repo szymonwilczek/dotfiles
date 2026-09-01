1d44b795b29318c249f104f4642ba8c7f93645a5
Szymon Wilczek <swilczek.lx@gmail.com>
2026-09-01 18:39:23 +0200

    emacs: tramp: optimize remote connection speed, caching, freeze prevention
    - Enable direct remote copying and terminal sanitization (tramp terminal type)
    - Add ControlMaster multiplexing integration with ~/.ssh/config and
      15m socket persistence
    - Disable remote lockfiles, auto-save roundtrips, and VC checks
      on remote buffers
    - Inhibit git-gutter and heavy hooks on remote buffers to eliminate
      UI freezes
    
    Signed-off-by: Szymon Wilczek <swilczek.lx@gmail.com>

--- emacs/.config/emacs/modules/tramp/tramp-mod.el
+++ emacs/.config/emacs/modules/tramp/tramp-mod.el
@@ -5,10 +5,42 @@
   :custom
   (tramp-default-method "ssh")
   (tramp-use-ssh-controlmaster-options nil)
-  (remote-file-name-inhibit-cache nil)
+  (tramp-use-scp-direct-remote-copying t)
+  (tramp-copy-size-limit (* 1024 1024))
+  (tramp-connection-timeout 10)
+  (tramp-terminal-type "tramp")
   (tramp-verbose 1)
+
+  ;; Caching and performance
+  (remote-file-name-inhibit-cache nil)
+  (tramp-completion-reread-directory-timeout 120)
+  (password-cache-expiry 3600)
+
+  ;; Lockfiles and autosave
+  (remote-file-name-inhibit-locks t)
+  (remote-file-name-inhibit-auto-save t)
+  (remote-file-name-inhibit-auto-save-visited t)
   (tramp-auto-save-directory (expand-file-name "tramp-autosave" user-emacs-directory))
+
   :config
+  (add-to-list 'tramp-remote-path 'tramp-own-remote-path)
+
+  ;; Disable VC on remote files
+  (setq vc-ignore-dir-regexp
+        (format "\\(%s\\)\\|\\(%s\\)"
+                vc-ignore-dir-regexp
+                tramp-file-name-regexp))
+
+  (defun my/tramp-optimize-remote-buffer ()
+    "Disable heavy background hooks on remote files to prevent UI freezes."
+    (when (file-remote-p (or buffer-file-name default-directory))
+      (setq-local vc-handled-backends nil)
+      (when (bound-and-true-p git-gutter-mode)
+        (git-gutter-mode -1))))
+
+  (add-hook 'find-file-hook #'my/tramp-optimize-remote-buffer)
+  (add-hook 'dired-mode-hook #'my/tramp-optimize-remote-buffer)
+
   (with-eval-after-load 'projectile
     (setq projectile-enable-caching t)))
 
