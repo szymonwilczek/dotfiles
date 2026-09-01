7c3f19196c999e84be3f61b2b78e942f19bba976
Szymon Wilczek <swilczek.lx@gmail.com>
2026-09-02 00:19:30 +0200

    emacs: writings: remove unused new and find writing shortcuts
    - Remove SPC w n and SPC w f shortcuts
    - Retain SPC w z for toggling Zen writing mode
    
    Signed-off-by: Szymon Wilczek <swilczek.lx@gmail.com>

--- emacs/.config/emacs/modules/writing/writings-keys.el
+++ emacs/.config/emacs/modules/writing/writings-keys.el
@@ -3,9 +3,7 @@
 (with-eval-after-load 'evil-keys
   (when (fboundp 'my-leader-def)
     (my-leader-def
-      "w"  '(:ignore t :which-key "Writings & Poetry")
-      "wz" '(my/writings-zen-toggle :which-key "Toggle Zen Mode")
-      "wn" '(my/writings-new :which-key "New Poem / Meditation")
-      "wf" '(my/writings-find :which-key "Find Writing / Poem"))))
+      "w"  '(:ignore t :which-key "Writings")
+      "wz" '(my/writings-zen-toggle :which-key "Toggle Zen Mode"))))
 
 (provide 'writings-keys)
