6861a30d9bf511d07db934c5c96c99d8be6764b8
Szymon Wilczek <swilczek.lx@gmail.com>
2026-09-18 14:42:10 +0200

    emacs: ui: showing special buffers
    Signed-off-by: Szymon Wilczek <swilczek.lx@gmail.com>

--- emacs/.config/emacs/modules/tabs/tabs-mod.el
+++ emacs/.config/emacs/modules/tabs/tabs-mod.el
@@ -4,9 +4,8 @@
   :demand t
   :custom
   (bufferline-separator-style 'vertical)
-  (bufferline-show-special-buffers nil)
   :config
-  (setq bufferline-show-special-buffers nil)
+  (setq bufferline-show-special-buffers t)
   (global-bufferline-mode 1))
 
 (require 'tabs-keys)
